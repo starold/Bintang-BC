d7dc2ea3bb1a9d3074c3d0f4341c91ef5a0b397d
Bintang <bintangp630@gmail.com>
2026-01-13 14:26:17 +0700

    W1 D1 "FooBar"

---
+++ Foo Bar/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Masukkan angka: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("FooBar");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Foo");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Bar");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
+
+
