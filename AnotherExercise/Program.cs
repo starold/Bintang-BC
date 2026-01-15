4c91c542984b1e281d1312754ae773d77be9289f
Bintang <bintangp630@gmail.com>
2026-01-15 08:29:39 +0700

    W2 D2

---
+++ AnotherExercise/Program.cs
@@ -0,0 +1,10 @@
+﻿class Student
+{
+    public string Name {get; set;}
+    public int Score {get; set;}
+    public string GetGrade()
+    {
+        if (Score >= 85) return "A";
+        
+    }
+}
