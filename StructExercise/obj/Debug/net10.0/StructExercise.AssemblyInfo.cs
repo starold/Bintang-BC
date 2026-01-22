2d5adb0faacc24bf01c5399ee780c1ed1915a4fa
Bintang <bintangp630@gmail.com>
2026-01-22 14:51:11 +0700

    W2 D4

--- StructExercise/obj/Debug/net10.0/StructExercise.AssemblyInfo.cs
+++ StructExercise/obj/Debug/net10.0/StructExercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StructExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2d52dc68d5edb2c92a5e3424d6626dfd423f1af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c571f57accf22b467cb42f1e058665c357f9d1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("StructExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StructExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
