3448474a678983a6e9a3f68478832d651dc2a178
Bintang <bintangp630@gmail.com>
2026-02-13 15:00:29 +0700

    W5 D5

--- DaftarSekolahCRUD/Infrastructure/Persistence/AppDbContext.cs
+++ DaftarSekolahCRUD/Infrastructure/Persistence/AppDbContext.cs
@@ -6,6 +6,12 @@ namespace DaftarSekolahCRUD.Infrastructure.Persistence
     public class AppDbContext : DbContext
     {
         public DbSet<Student> Students=> Set<Student>();
-        
+        public DbSet<StudentProfile> StudentProfiles => Set<StudentProfile>();
+        public DbSet<ClassRoom> ClassRooms=> Set<ClassRoom>();
+        public DbSet<Extracurricular> Extracurriculars=> Set<Extracurricular>();
+        public DbSet<StudentExtracurricular> StudentExtracurriculars=> Set<StudentExtracurricular>();
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options){}
     }
 }
